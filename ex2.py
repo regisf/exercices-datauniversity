d4bfc0d1fbe2d37490d77ff2b0f5f9ff683bc8dc
regisf <regisfloret@protonmail.com>
2019-02-09 11:34:05 +0100

    Add comments

--- ex2.py
+++ ex2.py
@@ -8,19 +8,22 @@ import csv
 # dans un fichier ouvert
 
 lines = [line for line in sys.stdin]
-# Utiliser les fichiers mémoires
 
+# TODO: Utiliser les fichiers mémoires
+# Ouvre un fichier et écrit la liste du stdin
 with open('my-data.csv', 'wt') as f:
     f.writelines(lines)
 
+# lit le fichier et donne le à csv;read pour créer 
 with open('my-data.csv', 'rt') as f:
     content = csv.reader(f)
+    # Popule un set de comprehension pour supprimer les doublons
+    setted = {line[9] for line in content}
 
-    for line in content:
-        print(line[9])
+    # Affiche les codes postaux
+    for s in setted:
+         print(s)
 
+# Supprime le fichier de travail
 os.unlink('my-data.csv')
 
-#print(lines)
-#for line in sys.stdin:
-#    print(line.strip().split(','))
